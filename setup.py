12b9af78967cee839746403a2684bf329baf15c3
Torsten Ostgard <stolentranslations@gmail.com>
2018-07-05 18:23:37 -0500

    Drop Python 2.7 support
    Python 2 is an annoying burden and an entirely unnecessary one, given
    that this project is designed to be a CLI program, not a library where
    broad compatibility is essential. Python 2.7 is still technically
    supported until 2020, but its death must come sooner.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
     author='Torsten Ostgard',
     url='https://github.com/torsten-ostgard/booru-note-copy',
     packages=setuptools.find_packages(exclude=['tests']),
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+    python_requires='>=3.4',
     install_requires=requires,
     entry_points={
         'console_scripts': [
@@ -35,8 +35,6 @@ setuptools.setup(
         'Intended Audience :: End Users/Desktop',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
